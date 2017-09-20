87bb680eea1fb3dff4659607b3e3c222984a28a9
Florent Lienard <f.lienard@tbs-education.org>
2017-09-20 17:39:45 +0200

    mailer

--- app/mailers/ordonnance_mailer.rb
+++ app/mailers/ordonnance_mailer.rb
@@ -3,7 +3,7 @@ class OrdonnanceMailer < ApplicationMailer
 def new_ordo(ordonnance)
     @ordonnance = ordonnance  # Instance variable => available in view
 
-    mail(to: 'contact@pharmacielienard.fr', subject: 'Welcome to Le Wagon')
+    # mail(to: 'contact@pharmacielienard.fr', subject: 'Welcome to Le Wagon')
     # This will render a view in `app/views/user_mailer`!
 
     mail(
