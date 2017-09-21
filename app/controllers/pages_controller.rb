719341c19b98a503dd623252f86ba8ace304b4b9
Florent Lienard <f.lienard@tbs-education.org>
2017-09-21 15:16:50 +0200

    gammes link routes and pages created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,26 @@ class PagesController < ApplicationController
   end
   def gammes
   end
+  def phytotherapie
+  end
+  def homeopathie
+  end
+  def gemmotherapie
+  end
+  def orthopedie
+  end
+  def puericulture
+  end
+  def materiel_medical
+  end
+  def complements_alimentaires
+  end
+  def dermatologie
+  end
+  def veterinaire
+  end
+  def incontinence
+  end
+  def optique
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,20 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'services', to: 'pages#services'
   get 'gammes', to: 'pages#gammes'
+  get 'phytotherapie', to: 'pages#phytotherapie'
+  get 'homeopathie', to: 'pages#homeopathie'
+  get 'gemmotherapie', to: 'pages#gemmotherapie'
+  get 'puericulture', to: 'pages#puericulture'
+  get 'orthopedie', to: 'pages#orthopedie'
+  get 'materiel_medical', to: 'pages#materiel_medical'
+  get 'complements_alimentaires', to: 'pages#complements_alimentaires'
+  get 'dermatologie', to: 'pages#dermatologie'
+  get 'veterinaire', to: 'pages#veterinaire'
+  get 'incontinence', to: 'pages#incontinence'
+  get 'veterinaire', to: 'pages#veterinaire'
+  get 'optique', to: 'pages#optique'
+
+
 
   Rails.application.routes.draw do
       resources :ordonnances, only: [:show, :index, :new, :create]
